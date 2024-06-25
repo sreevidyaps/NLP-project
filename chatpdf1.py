b1cc8b3b5eb300b617e6677e1f6ff32718a833b7
sreevidyaps <sreevidyasree2002@gmail.com>
2024-06-25 12:44:28 +0530

    ui update

--- chatpdf1.py
+++ chatpdf1.py
@@ -118,26 +118,13 @@ def main():
     # # raw_text = get_pdf_text(pdf_docs)
     # text_chunks = get_text_chunks(text)
     # get_vector_store(text_chunks)
-    st.set_page_config("Chat PDF")
-    st.header("Chat with PDF using Gemini💁")
-
-    user_question = st.text_input("Ask a Question from the PDF Files")
+    st.set_page_config("LawGenie")
+    st.header("LawGenie")
+    st.info("Your Virtual Legal Expert")
+    user_question = st.text_area("Ask your questions")
 
     if user_question:
         user_input(user_question)
        
-
-    # with st.sidebar:
-    #     st.title("Menu:")
-    #     pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
-    #     if st.button("Submit & Process"):
-    #         with st.spinner("Processing..."):
-                # raw_text = get_pdf_text(pdf_docs)
-                # text_chunks = get_text_chunks(raw_text)
-                # get_vector_store(text_chunks)
-    #             st.success("Done")
-
-
-
 if __name__ == "__main__":
     main()
